442ecbc596370b3a61867ddf148e26e147d503ae
JulianHalbmayr <julian.halbmayr@htlwy.at>
2025-05-05 12:21:26 +0200

    Zeige letzte 5 Fahrten des Benutzers auf der Startseite an
    Ermöglicht Anzeige kürzlicher Aktivitäten basierend auf den letzten Fahrten aus der Datenbank.
    Bindet die Route und das Fahrtdatum in die Darstellung ein.

--- Carpooling_Students/Pages/UserPage.cshtml.cs
+++ Carpooling_Students/Pages/UserPage.cshtml.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Carpooling_Students.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 public class UserPage_cs : PageModel
 {
@@ -13,6 +16,8 @@ public class UserPage_cs : PageModel
 
     public Person AngemeldetePerson { get; set; }
 
+    public List<Fahrt> LetzteFahrten { get; set; }
+
     public IActionResult OnGet()
     {
         int? userId = HttpContext.Session.GetInt32("UserId");
@@ -24,6 +29,14 @@ public class UserPage_cs : PageModel
 
         if (AngemeldetePerson == null)
             return RedirectToPage("/Login");
+
+        // Die letzten 5 Fahrten des Benutzers als Fahrer
+        LetzteFahrten = _context.Fahrten
+            .Include(f => f.Route)
+            .Where(f => f.Fahrer.PersonId == userId)
+            .OrderByDescending(f => f.StartDatum)
+            .Take(5)
+            .ToList();
 
         return Page();
     }
