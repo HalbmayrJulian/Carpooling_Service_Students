3f661d077491f5c519c503b69dc0223c7de0ac12
JulianHalbmayr <julian.halbmayr@htlwy.at>
2025-05-26 11:16:57 +0200

    Shop Seite überarbeitet

--- Carpooling_Students/Pages/Shop.cshtml.cs
+++ Carpooling_Students/Pages/Shop.cshtml.cs
@@ -1,10 +1,22 @@
+using Carpooling_Students.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Shop_cs : PageModel
 {
+    private readonly CarpoolContext _context;
+
+    public Shop_cs(CarpoolContext context)
+    {
+        _context = context;
+    }
+
+    public List<Item> ArtikelListe { get; set; } = new();
+    
+
     public void OnGet()
     {
-        // Optional: Coin-Stand aus Datenbank oder Session laden
-        // ViewData["Coins"] = ...
+        ArtikelListe = _context.Items.ToList();
     }
 }
