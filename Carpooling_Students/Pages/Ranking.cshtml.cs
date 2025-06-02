5670a1c82cc2465dab04f84fc62f329ebb1dc0f7
Deiac.Gabriel <gabriel.deiac@htlwy.at>
2025-06-02 10:39:48 +0200

    Ranking erneut wegen Konflikt hochgeladen

--- Carpooling_Students/Pages/Ranking.cshtml.cs
+++ Carpooling_Students/Pages/Ranking.cshtml.cs
@@ -1,9 +1,41 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Carpooling_Students.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 public class Ranking_cs : PageModel
 {
+    private readonly CarpoolContext _context;
+
+    public Ranking_cs(CarpoolContext context)
+    {
+        _context = context;
+    }
+
+    public List<Person> PersonenMitRanking { get; set; } = new();
+    public Person? AktuellerBenutzer { get; set; }
+    public int? AktuellerRang { get; set; }
+
     public void OnGet()
     {
-        // Optional: Logik zum Laden dynamischer Rangdaten aus der DB
+        int? userId = HttpContext.Session.GetInt32("UserId");
+
+        PersonenMitRanking = _context.Personen.Include(p => p.GefahreneFahrten)
+            .OrderByDescending(p => p.GefahreneFahrten.Count)
+            .ToList();
+
+        if (userId.HasValue)
+        {
+            AktuellerBenutzer = PersonenMitRanking
+                .FirstOrDefault(p => p.PersonId == userId.Value);
+
+            if (AktuellerBenutzer != null)
+            {
+                AktuellerRang = PersonenMitRanking.IndexOf(AktuellerBenutzer) + 1;
+            }
+        }
     }
 }
